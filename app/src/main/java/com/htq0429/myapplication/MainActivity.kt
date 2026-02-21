0eb54f1235b0639f653d19467be6f12e1b922b5b
HoTienQuoc0429 <hotienquoc0429@gmail.com>
2026-02-21 20:52:46 +0700

    Initial commit

---
+++ app/src/main/java/com/htq0429/myapplication/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.htq0429.myapplication
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val myTextView: TextView = findViewById(R.id.myTextView)
+    }
+}
